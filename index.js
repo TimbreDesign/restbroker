44fcaed454fa4725d87565dc8fa73e0d94d55f5c
Mikael Lindqvist <li.mikael@gmail.com>
2020-10-08 12:58:18 +0800

    module

---
+++ index.js
@@ -0,0 +1,7 @@
+const RestBrokerServer=require("./src/RestBrokerServer");
+const RestBrokerClient=require("./src/RestBrokerClient");
+
+module.exports={
+	Server: RestBrokerServer,
+	Client: RestBrokerClient
+};
